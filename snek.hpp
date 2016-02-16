25346b8f3ac50f7de270ec77262e1ea2e1841545
Samuel Sleight <samuel.sleight@gmail.com>
2016-02-16 23:52:28 +0000

    Refactor for future convenience
    also added throw() specifications

--- snek.hpp
+++ snek.hpp
@@ -5,8 +5,6 @@
 #ifndef SNEK_SNEK_HPP
 #define SNEK_SNEK_HPP
 
-#include <dlfcn.h>
-
 #include <cstdlib>
 #include <exception>
 
@@ -32,7 +30,66 @@ struct SymbolLoadError : public std::exception {
     const char* message;
 };
 
+void* load_library(const char* library) throw(LibraryLoadError);
+
+void unload_library(void* handle) noexcept;
+
+template<typename T>
+T load_symbol(void* handle, const char* symbol) throw(SymbolLoadError);
+
 }
+
+#ifdef _WIN32
+
+namespace snek {
+
+void* load_library(const char* library) throw(LibraryLoadError) {
+    throw LibraryLoadError("Not yet implemented on Windows");
+}
+
+template<typename T>
+T load_symbol(void* handle, const char* symbol) throw(SymbolLoadError) {
+    throw SymbolLoadError("Not yet implemented on Windows");
+}
+
+void unload_library(void* handle) noexcept {
+
+}
+
+}
+
+#else
+
+#include <dlfcn.h>
+
+namespace snek {
+
+void* load_library(const char* library) throw(LibraryLoadError) {
+    void* handle = dlopen(library, RTLD_LAZY);
+    if (!handle) {
+        throw LibraryLoadError(dlerror());
+    }
+
+    return handle;
+}
+
+template<typename T>
+T load_symbol(void* handle, const char* symbol) throw(SymbolLoadError) {
+    void* sym = dlsym(handle, symbol);
+    if (!sym) {
+        throw SymbolLoadError(dlerror());
+    }
+
+    return reinterpret_cast<T>(sym);
+}
+
+void unload_library(void* handle) noexcept {
+    dlclose(handle);
+}
+
+}
+
+#endif
 
 #define SNEK_DECLARE_TYPEDEFS(name, ret, ...) \
 typedef ret(*name ## _t)(__VA_ARGS__);
@@ -41,25 +98,19 @@ typedef ret(*name ## _t)(__VA_ARGS__);
 name ## _t name;
 
 #define SNEK_LOAD_MEMBER(name, ret, ...) \
-if(!(this->name = reinterpret_cast<name ## _t>(dlsym(handle, #name)))) { \
-    throw snek::SymbolLoadError(dlerror()); \
-}
+this->name = snek::load_symbol<name ## _t>(this->handle, #name);
 
 #define SNEK_EXPAND(name) \
 struct name { \
     SNEK_FIELDS(SNEK_DECLARE_TYPEDEFS) \
 \
-    name(const char* library) { \
-        handle = dlopen(library, RTLD_LAZY); \
-        if(!handle) { \
-            throw snek::LibraryLoadError(dlerror()); \
-        }\
-\
+    name(const char* library) throw(snek::LibraryLoadError, snek::SymbolLoadError) { \
+        this->handle = snek::load_library(library); \
         SNEK_FIELDS(SNEK_LOAD_MEMBER) \
     }; \
 \
     ~name() {\
-        dlclose(handle); \
+        snek::unload_library(this->handle); \
     } \
 \
     void* handle; \
